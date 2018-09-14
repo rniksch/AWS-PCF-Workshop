134c3744e368e4444556a1429e8af8459449fc8e
ryanik <ryanik@amazon.com>
2018-09-14 15:43:18 -0700

    removed redundant actions

--- modules/Lab-Setup/account-setup.sh
+++ modules/Lab-Setup/account-setup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 for i in Student{1..100}; do cf create-user $i Passw0rd; cf create-org $i; cf set-org-role $i $i OrgManager --stdin; done
-for i in Student{1:100}; do cf set-org-role $i $i OrgManager;  done
+for i in Student{1..100}; do cf set-org-role $i $i OrgManager;  done
